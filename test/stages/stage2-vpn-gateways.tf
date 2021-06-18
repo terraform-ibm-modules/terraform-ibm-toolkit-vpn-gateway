4862b1a20840b625b43bbb6261ffd55245968b1e
Sean Sundberg <seansund@us.ibm.com>
2021-06-18 17:17:43 -0500

    Updates module metadata
    - Removes direct dependency on vpc module
    - Adds `region` and `ibmcloud_api_key` with global scope to variables
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-vpn-gateways.tf
+++ test/stages/stage2-vpn-gateways.tf
@@ -4,7 +4,7 @@ module "vpn-gateways" {
   resource_group_id = module.resource_group.id
   region            = var.region
   ibmcloud_api_key  = var.ibmcloud_api_key
-  vpc_name          = module.vpc.name
+  vpc_name          = module.subnets.vpc_name
   vpc_subnet_count  = module.subnets.count
   vpc_subnets       = module.subnets.subnets
 }
