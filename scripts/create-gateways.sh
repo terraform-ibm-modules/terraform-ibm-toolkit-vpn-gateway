8791e5be93e607fc5a8b1b681ca9fc9e7c3aa7fc
Sean Sundberg <seansund@us.ibm.com>
2021-06-21 09:31:07 -0500

    Fixes iam token parsing
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/create-gateways.sh
+++ scripts/create-gateways.sh
@@ -15,7 +15,7 @@ if [[ -z "${JQ}" ]]; then
 fi
 
 IAM_RESULT=$(curl -s -X POST "https://iam.cloud.ibm.com/identity/token" -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=${IBMCLOUD_API_KEY}")
-IAM_TOKEN=$(echo "${IAM_RESULT}" | ${JQ} '.access_token')
+IAM_TOKEN=$(echo "${IAM_RESULT}" | ${JQ} -r '.access_token')
 
 if [[ -z "${IAM_TOKEN}" ]]; then
   echo "Error getting IAM_TOKEN"
