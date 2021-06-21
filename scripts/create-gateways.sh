04afd764113160dcfaaf77c00374d00e5039f57a
Sean Sundberg <seansund@us.ibm.com>
2021-06-21 08:54:06 -0500

    Updates token logging
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/create-gateways.sh
+++ scripts/create-gateways.sh
@@ -15,10 +15,12 @@ if [[ -z "${JQ}" ]]; then
 fi
 
 IAM_RESULT=$(curl -s -X POST "https://iam.cloud.ibm.com/identity/token" -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=${IBMCLOUD_API_KEY}")
-echo "IAM_RESULT: ${IAM_RESULT}"
-
 IAM_TOKEN=$(echo "${IAM_RESULT}" | ${JQ} '.access_token')
-echo "IAM_TOKEN: ${IAM_TOKEN}"
+
+if [[ -z "${IAM_TOKEN}" ]]; then
+  echo "Error getting IAM_TOKEN"
+  exit 1
+fi
 
 API_ENDPOINT="https://${REGION}.iaas.cloud.ibm.com/v1"
 API_VERSION="2021-06-18"
@@ -32,9 +34,7 @@ for id in subnet_ids; do
   name="${BASE_NAME}-${count}"
   echo "Provisioning $name VPN instance for subnet: $id"
 
-  RESULT=$(curl -s -H "Authorization: ${IAM_TOKEN}" \
-    -X POST "${API_ENDPOINT}/vpn_gateways?version=${API_VERSION}&generation=2" \
-    -d "{\"name\":\"${name}\",\"mode\":\"policy\",\"subnet\":{\"id\": \"$id\"},\"resource_group\":{\"id\":\"${RESOURCE_GROUP}\"}}")
+  RESULT=$(curl -s -H "Authorization: ${IAM_TOKEN}" -X POST "${API_ENDPOINT}/vpn_gateways?version=${API_VERSION}&generation=2" -d "{\"name\":\"${name}\",\"mode\":\"policy\",\"subnet\":{\"id\": \"$id\"},\"resource_group\":{\"id\":\"${RESOURCE_GROUP}\"}}")
 
   echo "Result: ${RESULT}"
 
