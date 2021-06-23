259c5a2dace62cb41d42645b0ec77f7a67268348
Sean Sundberg <seansund@us.ibm.com>
2021-06-23 09:37:39 -0500

    Adds logic to wait for VPN Gateways to be created
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/create-gateways.sh
+++ scripts/create-gateways.sh
@@ -5,6 +5,13 @@ RESOURCE_GROUP="$2"
 BASE_NAME="$3"
 SUBNET_IDS="$4"
 
+if [[ -z "${TMP_DIR}" ]]; then
+  TMP_DIR="./.tmp"
+fi
+mkdir -p "${TMP_DIR}"
+
+GATEWAY_IDS_FILE="${TMP_DIR}/vpn-gateway-ids-$(head /dev/urandom | LC_ALL=C tr -dc A-Za-z0-9 | head -c 5).txt"
+
 JQ=$(command -v jq | command -v ./bin/jq)
 
 if [[ -z "${JQ}" ]]; then
@@ -32,7 +39,37 @@ for id in $subnet_ids; do
   name="${BASE_NAME}-${count}"
   echo "Provisioning $name VPN instance for subnet: $id"
 
-  curl -s -H "Authorization: Bearer ${IAM_TOKEN}" -X POST "${API_ENDPOINT}/vpn_gateways?version=${API_VERSION}&generation=2" -d "{\"name\":\"${name}\",\"mode\":\"policy\",\"subnet\":{\"id\": \"$id\"},\"resource_group\":{\"id\":\"${RESOURCE_GROUP}\"}}"
+  create_result=$(curl -s -H "Authorization: Bearer ${IAM_TOKEN}" -X POST "${API_ENDPOINT}/vpn_gateways?version=${API_VERSION}&generation=2" -d "{\"name\":\"${name}\",\"mode\":\"policy\",\"subnet\":{\"id\": \"$id\"},\"resource_group\":{\"id\":\"${RESOURCE_GROUP}\"}}")
+
+  vpn_gateway_id=$(cat "$create_result" | ${JQ} -r '.id // empty')
+
+  echo "$vpn_gateway_id" >> "${GATEWAY_IDS_FILE}"
+
+  echo "Result of provisioning $name: $vpn_gateway_id"
+  echo "$create_result"
 
   count=$((count + 1))
 done
+
+GATEWAY_IDS_REGEX=$(cat "${GATEWAY_IDS_FILE}" | paste -sd "|" -)
+
+echo "Waiting for VPN Gateways to be created"
+
+count=0
+while [[ $count -lt 20 ]]; do
+  statuses=$(curl -s -X GET "${API_ENDPOINT}/v1/vpn_gateways/${vpn_gateway_id}?version=${API_VERSION}&generation=2" -H "Authorization: ${IAM_TOKEN}" | ${JQ} -r --args re "${GATEWAY_IDS_REGEX}" '.vpn_gateways[] | select(.id|test($re)) | .status')
+
+  echo "Statuses: $statuses"
+  if [[ $(echo "$statuses" | grep -c "pending") -eq 0 ]]; then
+    echo "VPN Gateways provisioned"
+    break
+  fi
+
+  count=$((count + 1))
+  echo "Waiting for VPN Gateways"
+  sleep 30
+done
+
+if [[ $count -eq 20 ]]; then
+  echo "Timed out waiting for VPN Gateways"
+fi
--- scripts/delete-gateways.sh
+++ scripts/delete-gateways.sh
@@ -4,6 +4,13 @@ REGION="$1"
 RESOURCE_GROUP="$2"
 SUBNET_IDS="$3"
 
+if [[ -z "${TMP_DIR}" ]]; then
+  TMP_DIR="./.tmp"
+fi
+mkdir -p "${TMP_DIR}"
+
+GATEWAY_IDS_FILE="${TMP_DIR}/vpn-gateway-ids-$(head /dev/urandom | LC_ALL=C tr -dc A-Za-z0-9 | head -c 5).txt"
+
 JQ=$(command -v jq | command -v ./bin/jq)
 
 if [[ -z "${JQ}" ]]; then
@@ -20,7 +27,7 @@ IAM_TOKEN=$(curl -s -X POST "https://iam.cloud.ibm.com/identity/token" \
 API_ENDPOINT="https://${REGION}.iaas.cloud.ibm.com"
 API_VERSION="2021-06-18"
 
-VPN_GATEWAYS=$(curl -X GET "${API_ENDPOINT}/v1/vpn_gateways?version=${API_VERSION}&generation=2&resource_group.id=${RESOURCE_GROUP}" -H "Authorization: Bearer ${IAM_TOKEN}")
+VPN_GATEWAYS=$(curl -s -X GET "${API_ENDPOINT}/v1/vpn_gateways?version=${API_VERSION}&generation=2&resource_group.id=${RESOURCE_GROUP}" -H "Authorization: Bearer ${IAM_TOKEN}")
 
 IFS=','
 subnet_ids=$SUBNET_IDS
@@ -28,24 +35,30 @@ for id in $subnet_ids; do
   echo "${VPN_GATEWAYS}" | ${JQ} -r --arg ID "${id}" '.vpn_gateways[] | select(.subnet.id == $ID) | .id' | \
     while read vpn_gateway_id;
   do
-    curl -X DELETE "${API_ENDPOINT}/v1/vpn_gateways/${vpn_gateway_id}?version=${API_VERSION}&generation=2" -H "Authorization: ${IAM_TOKEN}"
+    curl -s -X DELETE "${API_ENDPOINT}/v1/vpn_gateways/${vpn_gateway_id}?version=${API_VERSION}&generation=2" -H "Authorization: ${IAM_TOKEN}"
 
-    count=0
-    while [[ $count -lt 20 ]]; do
-      RESULT=$(curl -X GET "${API_ENDPOINT}/v1/vpn_gateways?version=${API_VERSION}&generation=2&resource_group.id=${RESOURCE_GROUP}" -H "Authorization: Bearer ${IAM_TOKEN}" | ${JQ} -r '.vpn_gateways[] | .id' | grep "$vpn_gateway_id")
-
-      if [[ -z "$RESULT" ]]; then
-        echo "VPN Gateway has been deleted: $vpn_gateway_id"
-        break
-      fi
-
-      count=$((count + 1))
-      echo "Waiting for VPN Gateway to be deleted: $vpn_gateway_id"
-      sleep 30
-    done
-
-    if [[ $count -eq 20 ]]; then
-      echo "Timed out waiting for VPN Gateway to be deleted"
-    fi
+    echo "$vpn_gateway_id" >> "${GATEWAY_IDS_FILE}"
   done
 done
+
+GATEWAY_IDS_REGEX=$(cat "${GATEWAY_IDS_FILE}" | paste -sd "|" -)
+
+echo "Waiting for VPN Gateways to be deleted"
+
+count=0
+while [[ $count -lt 20 ]]; do
+  ids=$(curl -s -X GET "${API_ENDPOINT}/v1/vpn_gateways?version=${API_VERSION}&generation=2&resource_group.id=${RESOURCE_GROUP}" -H "Authorization: Bearer ${IAM_TOKEN}" | ${JQ} -r '.vpn_gateways[] | .id' | grep -E "${GATEWAY_IDS_REGEX}" | paste -sd ";" -)
+
+  if [[ -z "$ids" ]]; then
+    echo "VPN Gateway have been deleted"
+    break
+  fi
+
+  count=$((count + 1))
+  echo "Waiting for VPN Gateways to be deleted: $ids"
+  sleep 30
+done
+
+if [[ $count -eq 20 ]]; then
+  echo "Timed out waiting for VPN Gateway to be deleted"
+fi
